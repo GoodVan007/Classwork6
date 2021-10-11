1c16a28d7f817a4ea0705c0c7706d8212e6b947d
Будаев Иван <budaevivan2003@gmail.com>
2021-10-11 19:42:54 +0500

    написал код

---
+++ ClassWork6/ClassWork6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите X:");
+            var x = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите Y:");
+            var y = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine($"!x = {!x}, !y = {!y}");
+            Console.WriteLine($"x & y = {x & y}, x | y, x^y = {x ^ y}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ classwork/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Linq;
+
+namespace classwork
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 2;
+            double g = 9.8;
+
+            Console.WriteLine(n);
+            Console.WriteLine(g);
+            Console.WriteLine(n++ + ++n);
+            Console.WriteLine(n);
+
+            int x = 3, y = 4, z = 5;
+            Console.WriteLine(x+y*z);
+
+            x = y += 2;
+            Console.WriteLine(x);
+
+            double angle = Math.PI/6;
+            double sinusX = Math.Sin(angle);
+            Console.WriteLine(sinusX);
+
+            //int k;
+            //k = 1;
+
+            var k = "1";
+            Console.WriteLine(k);
+
+            angle = x;
+            Console.WriteLine(angle);
+
+            x = (int)Math.Pow(2, 3);
+            Console.WriteLine(x);
+
+            x = (int)Math.Sqrt(3);
+            Console.WriteLine(x);
+
+            x = (int)Math.Round(Math.Sqrt(3));
+            Console.WriteLine(x);
+
+            Console.WriteLine((int)(-1.5));
+            Console.WriteLine(Math.Floor(-1.5));
+
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
