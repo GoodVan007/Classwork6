eb435d0015db00b0f4a883e7191c2508bf232048
Будаев Иван <budaevivan2003@gmail.com>
2021-11-15 13:40:55 +0500

    6.3 complited

---
+++ ClassWork Hw6.3/ClassWork Hw6.3/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork_Hw6._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите позицию белого ферзя");
+            var whiteQueenPosition = Console.ReadLine();
+
+            int queenX, queenY;
+            GetCoordinates(whiteQueenPosition, out queenY, out queenX);
+            Console.WriteLine($"{queenX} {queenY}");
+
+            Console.WriteLine("Введите позицию черной пешки");
+            var blackPawnPosition = Console.ReadLine();
+
+            if(!IsFiguresPositionsCorrect(whiteQueenPosition, blackPawnPosition))
+            {
+                Console.WriteLine("Позиции фигур не соответствуют условиям задачи");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Введите код белого ферзя");
+            var move = Console.ReadLine();
+
+            if (IsQueenCanMakeSafeMove(whiteQueenPosition, blackPawnPosition, move))
+
+                Console.WriteLine($"Ферзь {whiteQueenPosition} может безопастно пойти на клетку {move}");
+            else
+                Console.WriteLine($"Ферзь {whiteQueenPosition} не может пойти на эту клетку {move}");
+            
+           
+            Console.ReadKey();
+        }
+        static void GetCoordinates(string position, out int row, out int column)
+        {
+            row = (int)position[1] - 0x31;
+            column = (int)position[0] - 0x61;
+        }
+
+        static bool IsFiguresPositionsCorrect(string queenPosition, string pawnPosition)
+        {
+            return !IsEqualPositions(queenPosition, pawnPosition) &&
+                IsPawnPositionCorrect(pawnPosition) &&
+                !IsQueenStrikesPawn(queenPosition, pawnPosition) &&
+                !IsPawnStrikesQueen(queenPosition, pawnPosition);
+        }
+
+        static bool IsQueenCanMakeSafeMove(string queenPosition,string pawmPositions, string move)
+        {
+            return IsQueenCanMove(queenPosition, move) &&
+                !IsPawnStrikesQueen(move, pawmPositions);
+        }
+        static bool IsEqualPositions(string position1, string position2)
+        {
+            return position1 == position2;
+        }
+        static bool IsPawnStrikesQueen(string queenPosition, string pawnPosition)
+        {
+            int qx, qy, px, py;
+            GetCoordinates(queenPosition, out qy, out qx);
+            GetCoordinates(pawnPosition, out py, out px);
+            return py - qy == 1 && Math.Abs(px - qx) == 1;
+        }
+
+        static bool IsQueenStrikesPawn(string queenPosition, string pawnPosition)
+        {
+            return IsQueenCanMove(queenPosition, pawnPosition);
+        }
+
+        static bool IsPawnPositionCorrect(string pawnPosition)
+        {
+            int px, py;
+            GetCoordinates(pawnPosition, out py, out px);
+            return py > 0 && py < 7;
+        }
+
+        static bool IsQueenCanMove(string queenPosition, string move)
+        {
+            int qx, qy, mx, my;
+            GetCoordinates(queenPosition, out qy, out qx);
+            GetCoordinates(move, out my, out mx);
+            return qy == my || qx == mx || Math.Abs(qy - my) == Math.Abs(qx - mx);
+        }
+    }
+}
---
+++ ClassWork7/ClassWork7/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+
+namespace ClassWork7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("НАХУУУУЙ УрФУ с его дистантом и переобуванием(как у моих родителей)");
+            Console.ReadKey();
+            Console.WriteLine("Нахуй лок даун, сука гет даун. Ствол у виска клик-клэк, пиу-пау");
+            Console.WriteLine("Мама don`t Cry кого-то въебал, пару пинт лина и лаве на paypal");
+            Console.WriteLine("© Баранский В.А.");
+            Console.ReadKey();
+
+            Console.WriteLine("Введите начальное значение аргумента");
+            var x0 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение шага");
+            var step = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число строк таблицы");
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" x | x^2");
+            Console.WriteLine(new string('-', 15));
+
+            PrintHeader("x^2");
+
+            for(var i = 0; i < n; i++)
+            {
+                
+                Console.WriteLine($"{x0,5:F2} | {x0 * x0:F3}");
+                x0 += step;
+            }
+                        
+            PrintHeader("sin x");
+
+            for (var i = 0; i < n; i++)
+            {
+                var arg = x0 + step * i;
+                Console.WriteLine($"{arg + step * 1,5:F2} | {Math.Sin(arg)}");
+            }
+            
+            Console.ReadKey();
+        }
+        static void PrintHeader(string functionName)
+        {
+            Console.WriteLine(" x | x^2");
+            Console.WriteLine(new string('-', 15));
+        }
+ 
+
+    }
+}
